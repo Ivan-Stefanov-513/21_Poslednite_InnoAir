9df8a3d614de6afc9fda2de58d0bebc5c566709d
vl_garistov <vl_garistov@gmail.com>
2021-06-03 00:07:48 +0300

    Split the websocket ESP32 code into multiple files and started work on API

---
+++ ESP32_com_test/websocket.h
@@ -0,0 +1,43 @@
+#ifndef WEBSOCKET_H
+#define WEBSOCKET_H
+
+// This is commented because some idiot decided to not declare but to DEFINE functions in a header file
+// https://github.com/khoih-prog/WebSockets2_Generic/blob/master/src/WebSockets2_Generic_Client.hpp
+//#include <WebSockets2_Generic.h>
+#include <Arduino.h>
+
+typedef enum __attribute__((__packed__))
+{
+	echo_request,
+	echo_response,
+	humidity,
+	temperature,
+	noise,
+	dust
+}
+message_type_t;
+
+// TODO: figure out the correct types
+typedef union
+{
+	char echo[4];
+	uint16_t humidity;
+	uint16_t temperature;
+	float noise;
+	uint32_t dust;
+}
+message_data_t;
+
+typedef struct __attribute__((__packed__))
+{
+	message_type_t message_type;
+	message_data_t message_data;
+}
+message_t;
+
+//void onMessageCallback(websockets2_generic::WebsocketsMessage message);
+//void onEventsCallback(websockets2_generic::WebsocketsEvent event, String data);
+void wifi_init(void);
+bool websocket_init(void);
+
+#endif
