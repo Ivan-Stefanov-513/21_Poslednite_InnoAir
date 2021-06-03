f21bea7ab5ac0319e4527575aacb43c68160fac6
georgi159 <georgi_159@mail.bg>
2021-06-04 01:44:27 +0300

    more drawing

--- Web/chartDraw.js
+++ Web/chartDraw.js
@@ -48,6 +48,54 @@ config.options.plugins.annotation.annotations[config.AnoIndex]=
 	return config;
 }
 
+function addXLine(config, borderColor,borderWidth,yMIN,yMAX)
+{
+	if (!config.hasOwnProperty("options")) 
+	{
+		config.push
+		(
+		{
+
+			options: 
+			{
+				plugins: 
+				{
+					autocolors: false,
+					annotation: 
+					{
+						annotations: 
+						{
+
+						}
+
+					}
+				}
+			}
+		}
+		);
+	}
+	if (!config.hasOwnProperty("AnoIndex")) 
+	{
+		config.AnoIndex=0;
+	}
+	else 
+	{
+		config.AnoIndex++;
+	}
+// config.options.plugins.annotation.annotations.box3=[];
+config.options.plugins.annotation.annotations[config.AnoIndex]=		 
+{
+	drawTime: 'beforeDatasetsDraw',
+	type: 'line',
+	yMin: yMIN,
+	yMax: yMAX,
+	borderColor: borderColor,
+	borderWidth: borderWidth,				
+};
+	//config.options.plugins.annotation.annotations.push({key: "keys"});
+	// console.log(config);
+	return config;
+}
 
 
 function drawLineGraf(ctx) {
@@ -85,13 +133,7 @@ function drawLineGraf(ctx) {
 	    		{
 	    			annotations: 
 	    			{		
-	    				 line1: {
-				          type: 'line',
-				          yMin: 60,
-				          yMax: 60,
-				          borderColor: 'rgb(255, 99, 132)',
-				          borderWidth: 2,
-        					}
+
 	    			}
 
 	    		}
@@ -101,6 +143,8 @@ function drawLineGraf(ctx) {
 addBar(config,'rgba(2, 14, 132, 0.25)'   ,1,3,70,140);
 addBar(config,'rgba(255, 255, 0, 0.5)'   ,2,3,70,90);
 addBar(config,'rgba(255, 99, 132, 0.25)' ,1,2,50,70);
+
+addXLine(config,'rgb(255, 99, 132)', 2,90,90);
 
 var myChart = new Chart(ctx, config);
 
