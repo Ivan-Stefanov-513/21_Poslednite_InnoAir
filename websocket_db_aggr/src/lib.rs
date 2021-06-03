08de503c8ba109d68859758aa58cd92cf22b7f96
vl_garistov <vl_garistov@gmail.com>
2021-06-03 19:56:50 +0300

    got the secure websocket to work in the aggregation server. Needs some touch-ups

--- websocket_db_aggr/src/lib.rs
+++ websocket_db_aggr/src/lib.rs
@@ -1,15 +1,20 @@
 use clap::ArgMatches;
 use std::error::Error;
 use threadpool::ThreadPool;
-use std::sync::mpsc::channel;
+use std::fs::File;
+use std::io::{Read};
+use native_tls::{Identity, TlsAcceptor, TlsStream};
 use std::net::{TcpListener, TcpStream};
-use tungstenite::server::accept;
+use tungstenite::server;
 use tungstenite::protocol::WebSocket;
 use thiserror::Error;
 use mysql::PooledConn;
+use std::sync::Arc;
 
 const WEBSOCKET_BIND: &str = "10.21.42.2:1234";
 const DATABASE_ACCESS: &str = "mysql://poslednite:drenki_gl0g@localhost:3306/poslednite";
+const CERT_FILENAME: &str = "Poslednite_21.pkcs12";
+const CERT_PASS: &str = "drenki_gl0g";
 
 pub struct Config;
 
@@ -33,23 +38,36 @@ pub enum RuntimeError
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>
 {
+	let mut cert_file = File::open(CERT_FILENAME)?;
+	let mut pkcs12 = vec![];
+	cert_file.read_to_end(&mut pkcs12)?;
+	let pkcs12_identity = Identity::from_pkcs12(&pkcs12, CERT_PASS)?;
+
 	let websocket_server = TcpListener::bind(WEBSOCKET_BIND)?;
+	let acceptor = TlsAcceptor::new(pkcs12_identity)?;
+	let acceptor = Arc::new(acceptor);
+
 	let pool = mysql::Pool::new(DATABASE_ACCESS)?;
 	let n_worker_threads = 4;
 	let thread_pool = ThreadPool::new(n_worker_threads);
 
 	for client_stream in websocket_server.incoming()
 	{
+		// TODO: Refactor this, maybe move more stuff into the worker threads
 		if let Ok(client_stream) = client_stream
 		{
-			if let Ok(mut client_socket) = accept(client_stream)
+			let acceptor_clone = acceptor.clone();
+			if let Ok(client_stream) = acceptor_clone.accept(client_stream)
 			{
-				let mut db_conn = pool.get_conn()?;
-				thread_pool.execute(move||
+				if let Ok(mut client_socket) = server::accept(client_stream)
 				{
-					// TODO: Handle errors, possibly by sending result to the main thread?
-					handle_client(client_socket, db_conn);
-				});
+					let mut db_conn = pool.get_conn()?;
+					thread_pool.execute(move ||
+					{
+						// TODO: Handle errors, possibly by sending result to the main thread?
+						handle_client(client_socket, db_conn);
+					});
+				};
 			};
 		};
 	}
@@ -57,8 +75,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>
 	Ok(())
 }
 
-pub fn handle_client(mut client_socket: WebSocket<TcpStream>, mut db_conn: PooledConn) -> Result<(), Box<dyn Error>>
+pub fn handle_client(mut client_socket: WebSocket<TlsStream<TcpStream>>, mut db_conn: PooledConn) -> Result<(), Box<dyn Error>>
 {
-	println!("RAZGELE!");
+	println!("DEI, RABOTI!");
+	db_conn.query("INSERT INTO stations (name, location) VALUES ('test', 'here')")?;
 	Ok(())
 }
