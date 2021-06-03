e989732224fc18d7d1da0283936716c829e7f7b3
Ivan-Stefanov-513 <I{>
2021-06-03 03:31:35 +0300

    Added RMS AC signal measurement
    Changed microphone signal to use ADC1 instead of ADC2. Takes 10 samples and calculates signal AC RMS value.

--- ESP32_Noise_Meter/ESP32_Noise_Meter.ino
+++ ESP32_Noise_Meter/ESP32_Noise_Meter.ino
@@ -1,13 +1,23 @@
-#define Debug_Pin 26
+#define Debug_Pin 23
 uint8_t debug = 0;
 
-#define Mic_Pin 25
+#define Mic_Pin 35
 
 uint16_t mic_sample = 0;
+uint16_t samples[10] = {0};
+uint8_t position = 0;
+bool refresh = false;
+uint32_t zero = 0, noise = 0;
+
+#define SAMPLES 10
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(Mic_Pin, INPUT);
+  Serial.begin(250000);
+  pinMode(Mic_Pin, ANALOG);
+  analogReadResolution(12);
+  analogSetWidth(12);
+  analogSetAttenuation(ADC_0db);
+  analogSetClockDiv(1);
 
   pinMode(Debug_Pin, OUTPUT);
   digitalWrite(Debug_Pin, LOW);
@@ -16,13 +26,42 @@ void setup() {
 void loop() {
 
   mic_sample = analogRead(Mic_Pin);
+  /*
   Serial.print(0);
   Serial.print(',');
   Serial.print(4100);
   Serial.print(',');
   Serial.println(mic_sample);
+*/
+  samples[position] = mic_sample;
+  position++;
+  if (position >= SAMPLES) {
+    position = 0;
+    refresh = true;
+  }
 
-  if(debug == 0) {
+  if (refresh == true) {
+    zero = 0;
+    for (uint8_t i = 0 ; i < SAMPLES ; i++) {
+      zero += samples[i];
+    }
+    zero /= SAMPLES;
+    for (uint8_t i = 0 ; i < SAMPLES ; i++) {
+      noise += sq(samples[i] - zero);
+    }
+    noise /= SAMPLES;
+    noise = sqrt(noise);
+
+    Serial.print(0);
+    Serial.print(',');
+    Serial.print(4100);
+    Serial.print(',');
+    Serial.print(zero);
+    Serial.print(',');
+    Serial.println(noise);
+  }
+
+  if (debug == 0) {
     digitalWrite(Debug_Pin, HIGH);
     debug = 1;
   }
